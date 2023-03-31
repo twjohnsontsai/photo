d7add22cab14ade64ca5a65fd8447055a9804319
twjohnsontsai <twjohnsontsai@icloud.com>
2023-03-31 23:29:08 +0800

    修改来源路径

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+<!--
+ * @Author: twjohnsontsai twjohnsontsai@icloud.com
+ * @Date: 2023-03-31 20:31:05
+ * @LastEditors: twjohnsontsai twjohnsontsai@icloud.com
+ * @LastEditTime: 2023-03-31 22:52:06
+ * @FilePath: /test/photo/README.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 这是一个简单的照片管理器
 可以将你历来杂乱没有管理好的照片档案，按日期分别建档分开
 只要有修改就可以进行有效的管理
--- photo.py
+++ photo.py
@@ -1,10 +1,18 @@
+'''
+Author: twjohnsontsai twjohnsontsai@icloud.com
+Date: 2023-03-31 14:53:05
+LastEditors: twjohnsontsai twjohnsontsai@icloud.com
+LastEditTime: 2023-03-31 23:28:34
+FilePath: /test/photo/photo.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import os
 import shutil
 from datetime import datetime
 from PIL import Image
 
 # 设置源文件夹路径
-src_folder = '/Volumes/photo'
+src_folder = '/Volumes/Mac/照片'
 
 # 设置目标文件夹路径
 dst_folder = '/Volumes/home/Photos/MobileBackup/photo'
