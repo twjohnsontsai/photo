1f755e6fee3005bc4451e63aad1c38bc510f3f26
twjohnsontsai <twjohnsontsai@icloud.com>
2023-03-31 23:35:15 +0800

    更改输入路径

--- photo.py
+++ photo.py
@@ -2,7 +2,7 @@
 Author: twjohnsontsai twjohnsontsai@icloud.com
 Date: 2023-03-31 14:53:05
 LastEditors: twjohnsontsai twjohnsontsai@icloud.com
-LastEditTime: 2023-03-31 23:28:34
+LastEditTime: 2023-03-31 23:29:09
 FilePath: /test/photo/photo.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
