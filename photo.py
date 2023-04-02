92285f04d598152b45fcb5c3a7c96744e4911dc2
twjohnsontsai <twjohnsontsai@icloud.com>
2023-04-02 09:37:22 +0800

    修改档案来源等路径

--- photo.py
+++ photo.py
@@ -1,3 +1,11 @@
+'''
+Author: twjohnsontsai twjohnsontsai@icloud.com
+Date: 2023-03-31 14:53:05
+LastEditors: twjohnsontsai twjohnsontsai@icloud.com
+LastEditTime: 2023-04-02 09:31:32
+FilePath: /photo/photo.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import os
 import shutil
 from datetime import datetime
